945054f0d791c555534a741f88bca04f5cebabca
olusholapacheco Pacheco <sholapacheco@gmail.com>
2024-05-07 12:08:42 +0000

    Inputs that shows numbers you typed when executed

--- main.py
+++ main.py
@@ -10,3 +10,8 @@ name = "Olivia"
 house_number = 41
 print(f"I am {name} and I live in house {house_number}.")
 
+# This allows you to print these two numbers and it shows these two numbers
+a = int(input("Enter a number"))
+b = int(input("Enter a number"))
+print(x)
+print(y)
